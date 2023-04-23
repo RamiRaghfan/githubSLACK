b6e7b1fd5c9bc17bf4d7adea25e06753f98f9c55
RamiRaghfan <rami.raghfan@gmail.com>
2023-04-23 20:15:28 +0300

    First

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const bp = require('body-parser');
+const app = express();
+const fetch = require('node-fetch');
+
+app.use(bp.json());
+
+app.get('/', (req, res) => { return res.send("Hello world");});
+
+app.post('/webhook', (req, res) => {
+    
+    let body = req.body;
+
+  
+
+
+
+    let branch = body.ref.replace('refs/heads/', '');
+
+    console.log(req.body + " BODYYY");
+    let added = body.head_commit.added.join(', ');
+    let modified = body.head_commit.modified.join(', ');
+    let removed = body.head_commit.removed.join(', ');
+    let commit_link = body.head_commit.url;
+
+    let message = `New Push \n\nBranch : ${branch}\nAdded : ${added}\n Modified : ${modified}\nRemoved : ${removed}\nLink : ${commit_link}`;
+
+    let options = {
+        method : "POST",
+        headers : {
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify({text : message})
+    }
+
+    fetch('https://hooks.slack.com/services/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', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      text: message
+    })
+  })
+  .then(response => {
+    console.log(response.body);
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+    
+    return "Working";
+    
+})
+
+
+
+
+app.listen(8080, ()=> {console.log("Server is running on port 8080")});
+
+
+
+
